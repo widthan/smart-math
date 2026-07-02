9bac93974bf3119df66a0db5b53718cce0d99049
Anatoliy Karpov <monolit.almaty@gmai.com>
2026-07-02 20:55:49 +0500

    chore: initialize Smart Math project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+
+import Container from "@/components/layout/Container/container";
+
+export default function HomePage() {
+  return (
+    <Container>
+      <h1 className="text-4xl font-bold">
+        Smart Math
+      </h1>
+    </Container>
+  );
+}
---
+++ components/layout/Container/container.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className="mx-auto w-full max-w-7xl px-4">
+      {children}
+    </div>
+  );
+}
